f2eea6178571dedf3fa7b2e74b335832531e8ca1
Bruno Duarte <53445564+bruno-duart@users.noreply.github.com>
2022-10-28 22:57:35 -0300

    Adicionando Requisições
    Foram criadas as requisições básicas. Inicialmente os dados são "armazenados" em listas em tempo de execução

---
+++ AluraFlix/AluraFlix/Data/VideosDTOS/ReadVideosDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AluraFlix.Data.VideosDTOS
+{
+    public class ReadVideosDTO
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "Título do Vídeo não pode exceder 50 caracteres")]
+        public string Titulo { get; set; }
+        [Required]
+        [StringLength(1000, ErrorMessage = "Descrição do Vídeo não pode exceder 1000 caracteres")]
+        public string Descricao { get; set; }
+        [Required]
+        public string Url { get; set; }
+    }
+}
---
+++ AluraFlix/AluraFlix/Profiles/VideosProfile.cs
@@ -0,0 +1,16 @@
+﻿using AluraFlix.Data.VideosDTOS;
+using AluraFlix.Models;
+using AutoMapper;
+
+namespace AluraFlix.Profiles
+{
+    public class VideosProfile: Profile
+    {
+        public VideosProfile()
+        {
+            CreateMap<CreateVideosDTO, Videos>();
+            CreateMap<Videos, ReadVideosDTO>();
+            CreateMap<UpdateVideosDTO, Videos>();
+        }
+    }
+}
