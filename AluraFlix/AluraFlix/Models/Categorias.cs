ed454ac567fe361a9e252255239e3b538a713371
Bruno Duarte <53445564+bruno-duart@users.noreply.github.com>
2022-11-24 21:32:12 -0300

    Criada classe Categorias
    Criada a classe Categorias, bem como seus DTOs e profile

---
+++ AluraFlix/AluraFlix/Models/Categorias.cs
@@ -0,0 +1,9 @@
+﻿namespace AluraFlix.Models
+{
+    public class Categorias
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Cor { get; set; }
+    }
+}
---
+++ AluraFlix/AluraFlix/Profiles/CategoriasProfile.cs
@@ -0,0 +1,16 @@
+﻿using AluraFlix.Data.VideosDTOS;
+using AluraFlix.Models;
+using AutoMapper;
+
+namespace AluraFlix.Profiles
+{
+    public class CategoriasProfile : Profile
+    {
+        public CategoriasProfile()
+        {
+            CreateMap<CreateVideosDTO, Categorias>();
+            CreateMap<Categorias, ReadVideosDTO>();
+            CreateMap<UpdateVideosDTO, Categorias>();
+        }
+    }
+}
